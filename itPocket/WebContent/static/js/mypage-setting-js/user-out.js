80bb066f5b9a8db20a602bc8459cf773c053469a
localhost05chh <programer.chh@gmail.com>
2023-04-23 11:50:23 +0900

    최현호 4/23

---
+++ itPocket/WebContent/static/js/mypage-setting-js/user-out.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+// jQuery 코드
+
+  // 초기 설정
+  $(".description-wrapper").hide();
+  $("#label").css("color", "#655dbb");
+
+  // label 클릭 시 이벤트 핸들러
+  $("label").click(function() {
+    var desc_id = $(this).attr("for"); 
+    var desc = $("#" + desc_id + ".description-wrapper");  
+
+    if (desc.is(":visible")) {
+      desc.hide(); 
+      $("#label").css("color", "black"); 
+    } else { 
+      $(".description-wrapper").hide();  
+      $("#label").css("color", "#655dbb");  
+      desc.show();
+      $("#label").css("color", "black"	); 
+    }
+  });
+	
